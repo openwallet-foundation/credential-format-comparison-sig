68e208ca72d8444e7d7533c61d63378e6bbae601
Mirko Mollik <mirkomollik@gmail.com>
2023-09-12 08:58:02 +0200

    BUG: checkout on windows system throws an error Fixes #54
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- viewer/tools/validate.mjs
+++ viewer/tools/validate.mjs
@@ -1,8 +1,11 @@
-import {readdirSync, readFileSync} from 'fs';
+import {readdirSync, readFileSync, rmSync, writeFileSync} from 'fs';
 import process from 'process';
 import { defFile, folder, schemaFolder, structureFile } from './values.mjs';
 import Ajv from 'ajv';
 import { join } from 'path';
+
+// pattern to match windows compliant file names
+const pattern = /^(?!^(PRN|AUX|CON|NUL|COM[1-9]|LPT[1-9])(\..*)?$)[^<>:"/\\|?*]+$/;
 
 let error = false;
 //will loop through all the files and will check if the entries match with the structure defined in the info.json file.
@@ -11,10 +14,14 @@ readdirSync(folder).forEach((subFolder) => {
     if(!infoFile) {
         console.log(`No schema file found for ${subFolder}`);
         process.exit(1);            
-    }    
+    }
     const ajv = new Ajv({allowUnionTypes: true});
     const validate = ajv.addSchema(JSON.parse(readFileSync(join(schemaFolder, defFile)))).compile(JSON.parse(infoFile));            
     readdirSync(`${folder}/${subFolder}`).filter(file => file !== structureFile).forEach((file) => {        
+        if(!pattern.test(file)) {
+            console.log(`File "../data/${subFolder}/${file}" does not match the pattern: ${pattern}}`);
+            error = true;           
+        }
         const content = JSON.parse(readFileSync(`${folder}/${subFolder}/${file}`));
         if(!validate(content)) {
             console.log(`File "../data/${subFolder}/${file}" is invalid`);
