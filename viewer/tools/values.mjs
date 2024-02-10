e1862cacd46b722dd0f2d888da76832fac6b79cc
Mirko <mirkomollik@gmail.com>
2024-02-10 11:57:12 +0100

    set schema reference to github hosted one
    Signed-off-by: Mirko <mirkomollik@gmail.com>

--- viewer/tools/values.mjs
+++ viewer/tools/values.mjs
@@ -3,3 +3,4 @@ export const schemaFolder = "../schemas";
 export const defFile = "defs.json";
 export const structureFile = 'schema.json';
 export const mergedStructure = './src/assets/structure.json';
+export const githubPath = 'https://raw.githubusercontent.com/openwallet-foundation/credential-format-comparison-sig/main';
