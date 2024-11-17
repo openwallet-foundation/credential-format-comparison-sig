8798ad3082da672b9a966241de6fd316253af533
Mirko Mollik <mirkomollik@gmail.com>
2024-11-17 12:56:17 +0100

    add info dialog (#116)
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- viewer/src/app/app.component.ts
+++ viewer/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MovedComponent } from './moved/moved.component';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,13 @@ import { AppService } from './app.service';
 export class AppComponent implements OnInit {
   tabs: string[] = [];
 
-  constructor(private appService: AppService) {}
+  constructor(private appService: AppService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.appService.getElements();
+
+    this.dialog.open(MovedComponent);
+
     this.tabs = Object.keys(this.appService.getElements()).filter(
       (key) => key !== 'Credential Profile' && key !== 'defs'
     );
